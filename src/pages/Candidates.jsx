e10d376051851c36fa09d4a48a6ec0e8b96f69d9
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2025-05-01 00:41:25 -0300

    if userdata = null navigate to /

--- src/pages/Candidates.jsx
+++ src/pages/Candidates.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 export default function Candidates() {
+    const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem("user"));
+
+    useEffect(() => {
+      if (!user) {
+        navigate("/");
+      }
+    }, [user, navigate]);
+
     return (
       <div className="p-4">
         <h1 className="text-2xl font-bold">Bienvenido, {user?.name}</h1>
